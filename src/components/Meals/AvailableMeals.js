a64f0c910a3779a18f1707d1c4a0c32cb283a6d4
Oleksii Zhabin <oleksii.zhabin@gmail.com>
2023-01-03 20:15:12 +0200

    firebase DB added. fetching data from DB

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,11 +1,36 @@
 import Card from "../UI/Card";
-import {DUMMY_MEALS} from "../../assets/dummyMeals";
+// import {DUMMY_MEALS} from "../../assets/dummyMeals";
 import styles from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
+import {useEffect, useState} from "react";
 
 const AvailableMeals = () => {
 
-    const mealsList = DUMMY_MEALS.map(meal => (
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+
+        const fetchMeals = async () => {
+            const response = await fetch('https://react-food-app-db-84d0b-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+            const responseData = await response.json();
+            // console.log(responseData);
+            const loadedMeals = [];
+
+            for (let key in responseData) {
+                loadedMeals.push({
+                    id:key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price
+                })
+            }
+            // console.log(loadedMeals)
+            setMeals(loadedMeals);
+        }
+        fetchMeals();
+    }, [])
+
+    const mealsList = meals.map(meal => (
         <MealItem
             key={meal.id}
             id={meal.id}
@@ -19,6 +44,7 @@ const AvailableMeals = () => {
         <Card>
             <ul>
                 {mealsList}
+                {mealsList.length === 0 && <p>No meals</p>}
             </ul>
         </Card>
     </section>
